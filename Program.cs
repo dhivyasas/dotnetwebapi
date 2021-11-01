75d3740c6fca001ac5c2396f7b8f9e68ea47c4fd
dhivyasas <93305852+dhivyasas@users.noreply.github.com>
2021-11-01 15:25:03 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,25 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnetwebapi.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace dotnetwebapi
 {
-    public class Program
+    public class Startup
     {
-        public static void Main(string[] args)
+        public Startup(IConfiguration configuration)
         {
-            CreateHostBuilder(args).Build().Run();
+            Configuration = configuration;
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddOpenApiDocument();
+             services.AddDbContext<FruitContext>(opt => opt.UseInMemoryDatabase("FruitList"));
+            //services.AddSwaggerGen(c =>
+            //{
+                //c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnetwebapi", Version = "v1" });
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //Accept All HTTP Request Methods from all origins
+app.UseCors(builder =>
+builder.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnetwebapi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+        }
     }
 }
